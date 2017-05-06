7487093de97adb6d89330287edddc4bc0c977dcb
LONG <v5zhu@qq.com>
2017-05-06 16:28:49 +0800

    更新阿里云短信网关接口

--- src/main/java/com/vvboot/end/thirdparty/api/aliyun/AliyunSmsGateway.java
+++ src/main/java/com/vvboot/end/thirdparty/api/aliyun/AliyunSmsGateway.java
@@ -1,13 +1,18 @@
 package com.vvboot.end.thirdparty.api.aliyun;
 
 import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
+import com.aliyun.mns.client.CloudAccount;
+import com.aliyun.mns.client.CloudTopic;
+import com.aliyun.mns.client.MNSClient;
+import com.aliyun.mns.common.ServiceException;
+import com.aliyun.mns.model.BatchSmsAttributes;
+import com.aliyun.mns.model.MessageAttributes;
+import com.aliyun.mns.model.RawTopicMessage;
+import com.aliyun.mns.model.TopicMessage;
+import com.vvboot.end.core.exception.InnerException;
+import com.vvboot.end.utils.PropertiesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /*
  * ============================================================================		
@@ -25,32 +30,77 @@ import java.util.Map;
  * ============================================================================		
  */
 public class AliyunSmsGateway {
+    private static final String fileName = "application.sms.properties";
+    private static String $AccessId;
+    private static String $AccessKey;
+    private static String $MNSEndpoint;
+    private static String $Topic;
+    private static String $SignName;
+    private static String $SMSTemplateCode;
+
+    static {
+        $AccessId = PropertiesUtil.getValue(fileName, "$AccessId");
+        $AccessKey = PropertiesUtil.getValue(fileName, "$AccessKey");
+        $MNSEndpoint = PropertiesUtil.getValue(fileName, "$MNSEndpoint");
+        $Topic = PropertiesUtil.getValue(fileName, "$Topic");
+        $SignName = PropertiesUtil.getValue(fileName, "$SignName");
+        $SMSTemplateCode = PropertiesUtil.getValue(fileName, "$SMSTemplateCode");
+    }
+
     private static final Logger logger = LoggerFactory.getLogger(AliyunSmsGateway.class);
 
-    public static final boolean batchSend(String url, String path, String method, String auth, String vars, String phones, String signName, String smsTemplate) {
-        Map<String, String> headers = new HashMap<>();
-        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
-        headers.put("Authorization", auth);
-        Map<String, String> querys = new HashMap<>();
-        querys.put("ParamString", vars);
-        querys.put("RecNum", phones);
-        querys.put("SignName", signName);
-        querys.put("TemplateCode", smsTemplate);
-        String result = null;
+    public static final boolean batchSend(String phone, String code) {
+        /**
+         * Step 1. 获取主题引用
+         */
+        CloudAccount account = new CloudAccount($AccessId, $AccessKey, $MNSEndpoint);
+        MNSClient client = account.getMNSClient();
+        CloudTopic topic = client.getTopicRef($Topic);
+        /**
+         * Step 2. 设置SMS消息体（必须）
+         *
+         * 注：目前暂时不支持消息内容为空，需要指定消息内容，不为空即可。
+         */
+        RawTopicMessage msg = new RawTopicMessage();
+        msg.setMessageBody("sms-message");
+        /**
+         * Step 3. 生成SMS消息属性
+         */
+        MessageAttributes messageAttributes = new MessageAttributes();
+        BatchSmsAttributes batchSmsAttributes = new BatchSmsAttributes();
+        // 3.1 设置发送短信的签名（SMSSignName）
+        batchSmsAttributes.setFreeSignName($SignName);
+        // 3.2 设置发送短信使用的模板（SMSTempateCode）
+        batchSmsAttributes.setTemplateCode($SMSTemplateCode);
+        // 3.3 设置发送短信所使用的模板中参数对应的值（在短信模板中定义的，没有可以不用设置）
+        BatchSmsAttributes.SmsReceiverParams smsReceiverParams = new BatchSmsAttributes.SmsReceiverParams();
+        smsReceiverParams.setParam("code", code);
+        smsReceiverParams.setParam("time", "15");
+        // 3.4 增加接收短信的号码
+        batchSmsAttributes.addSmsReceiver(phone, smsReceiverParams);
+//        batchSmsAttributes.addSmsReceiver("13880298929", smsReceiverParams);
+        messageAttributes.setBatchSmsAttributes(batchSmsAttributes);
         try {
-            HttpResponse response = HttpUtils.doGet(url, path, method, headers, querys);
-            result = EntityUtils.toString(response.getEntity());
-            //获取response的body
-            logger.info("<<======发送短信验证码响应:[{}]",result);
-            JSONObject obj = JSONObject.parseObject(result);
-            if("true".equals(obj.getString("success"))){
-                return true;
-            }else{
-                return false;
-            }
+            /**
+             * Step 4. 发布SMS消息
+             */
+            TopicMessage ret = topic.publishMessage(msg, messageAttributes);
+            logger.info("网关发送短信返回:[{}]", JSONObject.toJSONString(ret));
+        } catch (ServiceException se) {
+            System.out.println(se.getErrorCode() + se.getRequestId());
+            System.out.println(se.getMessage());
+            se.printStackTrace();
+            throw new InnerException("发送短信异常");
         } catch (Exception e) {
             e.printStackTrace();
+            throw new InnerException("发送短信异常");
+        } finally {
+            client.close();
         }
-        return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        batchSend("13880298929", "123456");
     }
 }
--- src/main/java/com/vvboot/end/utils/SmsUtil.java
+++ src/main/java/com/vvboot/end/utils/SmsUtil.java
@@ -1,6 +1,5 @@
 package com.vvboot.end.utils;
 
-import com.alibaba.fastjson.JSONObject;
 import com.vvboot.end.core.exception.InnerException;
 import com.vvboot.end.thirdparty.api.aliyun.AliyunSmsGateway;
 
@@ -23,20 +22,8 @@ public class SmsUtil {
 
     public static final int SMS_CODE_EXPIRED = 15;
 
-    //阿里云短信服务
-    public static final String SMS_GATEWAY_ALIYUN_URL = "http://sms.market.alicloudapi.com";
-    public static final String SMS_GATEWAY_ALIYUN_PATH = "/singleSendSms";
-    public static final String SMS_GATEWAY_ALIYUN_METHOD = "GET";
-    public static final String SMS_GATEWAY_ALIYUN_AUTH = "APPCODE 0b4cc9f202234a81b22507209358c05f";
-    public static final String SMS_GATEWAY_ALIYUN_SIGNNAME = "微微布特";
-    public static final String SMS_GATEWAY_ALIYUN_SMSTEMPLATE = "SMS_49260144";
-
-
     public static void sendSmsCode(String phone, String code) throws InnerException {
-        JSONObject vars = new JSONObject();
-        vars.put("code", code);
-        vars.put("time", SMS_CODE_EXPIRED);
-        boolean res = AliyunSmsGateway.batchSend(SMS_GATEWAY_ALIYUN_URL, SMS_GATEWAY_ALIYUN_PATH, SMS_GATEWAY_ALIYUN_METHOD, SMS_GATEWAY_ALIYUN_AUTH, vars.toJSONString(), phone, SMS_GATEWAY_ALIYUN_SIGNNAME, SMS_GATEWAY_ALIYUN_SMSTEMPLATE);
+        boolean res = AliyunSmsGateway.batchSend(phone, code);
         if (!res) {
             throw new InnerException("发送短信失败");
         }
