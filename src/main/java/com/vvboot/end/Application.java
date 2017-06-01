22010ca6d205d24190bd37731b61b039bdb93456
v5zhu@qq.com <longzhu20*>
2017-06-01 17:06:10 +0800

    提交定时任务

--- src/main/java/com/vvboot/end/Application.java
+++ src/main/java/com/vvboot/end/Application.java
@@ -2,12 +2,14 @@ package com.vvboot.end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author zhuxl@paxsz.com
  * @time 2017/5/4
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
