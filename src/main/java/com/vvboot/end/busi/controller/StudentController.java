a2b4036059eea7debea7db0b5f78c8e8dad8e45f
v5zhu@qq.com <longzhu20*>
2017-06-20 13:57:37 +0800

    培训记录接口

---
+++ src/main/java/com/vvboot/end/busi/controller/StudentController.java
@@ -0,0 +1,75 @@
+package com.vvboot.end.busi.controller;
+
+import com.alibaba.fastjson.JSONArray;
+import com.vvboot.end.busi.entity.SchoolOrganization;
+import com.vvboot.end.busi.entity.Student;
+import com.vvboot.end.busi.params.PageParam;
+import com.vvboot.end.busi.service.SchoolOrganizationService;
+import com.vvboot.end.busi.service.StudentService;
+import com.vvboot.end.core.commons.Pageable;
+import com.vvboot.end.core.commons.Success;
+import com.vvboot.end.core.exception.CoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by zhuxl@paxsz.com on 2016/7/27.
+ */
+@SuppressWarnings("ALL")
+@Controller
+@RequestMapping(value = "/api/v1/student")
+public class StudentController {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(value = "save", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity add(@RequestBody Student student) {
+        try {
+            studentService.addStudent(student);
+            Success ok = new Success("添加成功", "恭喜你!添加成功");
+            return new ResponseEntity(ok, HttpStatus.OK);
+        } catch (CoreException e) {
+            return new ResponseEntity(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "dropdown", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity dropdown() {
+        try {
+            JSONArray array = studentService.dropdown();
+            Success ok = new Success(array, "查询成功");
+            return new ResponseEntity(ok, HttpStatus.OK);
+        } catch (CoreException e) {
+            return new ResponseEntity(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity pagelist(@RequestBody PageParam pageParam) {
+        try {
+            Pageable pageable = studentService.studentList(pageParam);
+            Success ok = new Success(pageable, "查询成功");
+            return new ResponseEntity(ok, HttpStatus.OK);
+        } catch (CoreException e) {
+            return new ResponseEntity(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
