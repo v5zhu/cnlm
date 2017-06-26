de3dd9950fa1baa566ae22c4d8f9cb98672191a1
LONG <v5zhu@qq.com>
2017-06-27 00:30:23 +0800

    新增用户类

---
+++ src/main/java/net/leebao/auth/entity/User.java
@@ -0,0 +1,23 @@
+package net.leebao.auth.entity;
+
+import java.util.Date;
+
+/**
+ * Created by LONG on 2017/6/27.
+ */
+public class User {
+    private Long id;
+    private String uid;
+    private Long orgId;
+    private String loginName;
+    private String password;
+    private String name;
+    private String phone;
+    private String email;
+    private Date createTime;
+    private Date loginTime;
+    private Date lastLoginTime;
+    private Long count;
+
+//    http://blog.csdn.net/chexlong/article/details/37697555/
+}
